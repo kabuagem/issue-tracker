30e1f8771597159299be8f4a8888ec9ad28ebbe2
mkabuage <michael.kabuage@gmail.com>
2024-06-04 05:13:27 +0300

    Add assigned issues to prisma schema

---
+++ prisma/migrations/20240604021154_assigned_issues/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `Authenticator` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `Authenticator` DROP FOREIGN KEY `Authenticator_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `Issue` ADD COLUMN `assignedToUserId` VARCHAR(255) NULL;
+
+-- AlterTable
+ALTER TABLE `User` DROP COLUMN `createdAt`,
+    DROP COLUMN `updatedAt`;
+
+-- DropTable
+DROP TABLE `Authenticator`;
+
+-- AddForeignKey
+ALTER TABLE `Issue` ADD CONSTRAINT `Issue_assignedToUserId_fkey` FOREIGN KEY (`assignedToUserId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
